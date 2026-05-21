f10a8536b9cf7fdf35c64b6a0967e3fdfa61cf01
Anubhav <anubhav1121singh@gmail.com>
2026-05-21 18:10:27 +0530

    Initial Spring Boot setup

---
+++ src/main/java/paybandhu/PaybandhuApplication.java
@@ -0,0 +1,13 @@
+package paybandhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaybandhuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaybandhuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/paybandhu/PaybandhuApplicationTests.java
@@ -0,0 +1,13 @@
+package paybandhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaybandhuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
