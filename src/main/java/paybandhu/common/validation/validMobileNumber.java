6ce7d0ece049962622145b4597f7061416eabcc4
Anubhav <anubhav1121singh@gmail.com>
2026-05-24 13:43:47 +0530

    ValidMobileNumber Implementing..

--- src/main/java/paybandhu/common/validation/validMobileNumber.java
+++ src/main/java/paybandhu/common/validation/validMobileNumber.java
@@ -1,4 +1,10 @@
 package paybandhu.common.validation;
 
+import jakarta.validation.Constraint;
+
+import java.lang.annotation.Documented;
+
+@Documented
+@Constraint(validatedBy = MobileNumberValidator.class)
 public @interface validMobileNumber {
 }
