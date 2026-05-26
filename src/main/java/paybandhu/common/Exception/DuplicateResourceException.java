4520cd1537e13f8b54e959508e92ece48110230f
Anubhav <anubhav1121singh@gmail.com>
2026-05-26 22:39:14 +0530

    Added agentResponse , agentRepository, Global Exceptions , modified AgentDocument

---
+++ src/main/java/paybandhu/common/Exception/DuplicateResourceException.java
@@ -0,0 +1,9 @@
+package paybandhu.common.Exception;
+
+public class DuplicateResourceException extends RuntimeException{
+
+    public DuplicateResourceException(String message){
+        super(message);
+    }
+
+}
