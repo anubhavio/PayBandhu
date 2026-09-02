04b67b6a22a4c78acecd16931ee3754624b5c076
Anubhav <anubhav1121singh@gmail.com>
2026-09-02 18:28:28 +0530

    Implemented Retailer getById

--- src/main/java/paybandhu/retailer/api/response/RetailerResponse.java
+++ src/main/java/paybandhu/retailer/api/response/RetailerResponse.java
@@ -12,7 +12,11 @@ public class RetailerResponse {
 
     private Long id;
 
+    private String firstName;
+
     private String retailerCode;
+
+    private String mobileNumber;
 
     private RetailerStatus status;
 
