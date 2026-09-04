98dc89659d72393afeb0e60cb93291d0311143cb
Anubhav <anubhav1121singh@gmail.com>
2026-09-04 21:26:40 +0530

    Implemented SecurityFilterChain first phase

--- src/main/java/paybandhu/security/config/SecurityConfiguration.java
+++ src/main/java/paybandhu/security/config/SecurityConfiguration.java
@@ -2,8 +2,11 @@ package paybandhu.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
@@ -13,4 +16,16 @@ public class SecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)
+    throws Exception{
+
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return httpSecurity.build();
+    }
 }
