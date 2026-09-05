023771f7f4c6d3d2d56fcac7fee79e2a0e39a4ba
Anubhav <anubhav1121singh@gmail.com>
2026-09-05 21:49:11 +0530

    Implemented AuthenticationManger

--- src/main/java/paybandhu/security/config/SecurityConfiguration.java
+++ src/main/java/paybandhu/security/config/SecurityConfiguration.java
@@ -2,11 +2,14 @@ package paybandhu.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 public class SecurityConfiguration {
@@ -27,5 +30,12 @@ public class SecurityConfiguration {
                         .anyRequest().authenticated()
                 );
         return httpSecurity.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+       AuthenticationConfiguration configuration
+            ) throws Exception{
+        return configuration.getAuthenticationManager();
     }
 }
