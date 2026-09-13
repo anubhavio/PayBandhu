2de27f4c532c1a11cab6110261b724e532d295bb
Anubhav <anubhav1121singh@gmail.com>
2026-09-13 12:01:18 +0530

    Fixed the bug. A mobile number can't exceed length of 15,  as International format.

---
+++ src/main/java/paybandhu/agent/api/AgentApplicationController.java
@@ -0,0 +1,30 @@
+package paybandhu.agent.api;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import paybandhu.agent.api.request.AgentApplicationRequest;
+import paybandhu.agent.api.response.AgentApplicationResponse;
+import paybandhu.agent.service.AgentApplicationService;
+
+@RestController
+@RequestMapping("/api/agent-applications")
+@RequiredArgsConstructor
+public class AgentApplicationController {
+
+    private final AgentApplicationService agentApplicationService;
+
+    @PostMapping
+    public ResponseEntity<AgentApplicationResponse> submitApplication(
+            @Valid @RequestBody AgentApplicationRequest agentApplicationRequest
+            ){
+
+        AgentApplicationResponse response =
+                agentApplicationService.submitApplication(agentApplicationRequest);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(response);
+    }
+}
---
+++ src/main/java/paybandhu/agent/domain/AgentApplication.java
@@ -0,0 +1,82 @@
+package paybandhu.agent.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "agent_applications")
+@Entity
+public class AgentApplication {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "application_number", length = 30, unique = true, nullable = false)
+    private String applicationNumber;
+
+    @Column(name = "first_name", length = 50,  nullable = false)
+    private String firstName;
+
+    @Column(name = "middle_name", length = 50)
+    private String middleName;
+
+    @Column(name = "last_name", length = 50, nullable = false)
+    private String lastName;
+
+    @Column(name = "mobile_number", length = 15, nullable = false, unique = true)
+    private String mobileNumber;
+
+    @Column(name = "email_address", length = 150, unique = true, nullable = false)
+    private String emailAddress;
+
+    @Column(name = "aadhaar_number", length = 15, unique = true, nullable = false)
+    private String aadhaarNumber;
+
+    @Column(name = "pan_number",length = 50, unique = true, nullable = false)
+    private String panNumber;
+
+    @Column(name = "date_of_birth", nullable = false)
+    private LocalDate dateOfBirth;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id", nullable = false)
+    private Address address;
+
+    @Column(name = "registration_ip" , nullable = false)
+    private String registrationIp;
+
+    @Embedded
+    private RegistrationLocation registrationLocation;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AgentApplicationStatus status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "rejection_reason")
+    private AgentRejectionReason agentRejectionReason;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+}
